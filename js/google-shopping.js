b8fa3c4859f0d2e0c7f42ec210e43c41c2efbd8c
mullfuchs <burke.leo@gmail.com>
2016-07-20 17:04:20 -0700

    Finished deliverable

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,36 @@
 var data = require('../products.json');
 
 // Write your solutions below
+var itemCount = 0;
+for(var i = 0; i < data['items'].length; i++){
+	if(data['items'][i]['kind'] == "shopping#product"){
+		//itemCount++;
+	}
 
-console.log(data.items);
+	if(data.items[i].product.inventories[0].availability == "backorder"){
+		//console.log(data.items[i].product.title, " on backorder");
+	}
+
+	if(data.items[i].product.images.length > 1){
+		//console.log(data.items[i].product.title, "has multiple images");	
+	}
+
+	if(data.items[i].product.brand == "Canon"){
+		//console.log(data.items[i].product.title);	
+	}
+
+	if(data.items[i].product.author.name == "eBay"){
+		if(data.items[i].product.brand == "Canon"){
+			//console.log(data.items[i].product.title);			
+		}
+	}
+
+	console.log(data.items[i].product.brand);
+	console.log("$",data.items[i].product.inventories[0].price);
+	console.log(data.items[i].product.images[0].link);
+
+}
+
+console.log("Item count via json field" , data.currentItemCount);
+
+//console.log(data.items);
